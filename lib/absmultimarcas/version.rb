2e8cc86a42590b230a8a236289783d6a0fb0d0dc
Thiago Youssef <thiagodyoussef@gmail.com>
2021-09-16 13:36:56 -0300

    Init gem

---
+++ lib/absmultimarcas/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Absmultimarcas
+  VERSION = "0.1.0"
+end
---
+++ spec/absmultimarcas_spec.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+RSpec.describe Absmultimarcas do
+  it "has a version number" do
+    expect(Absmultimarcas::VERSION).not_to be nil
+  end
+end
