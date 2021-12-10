2af8dcee2fb146d4089c819a5094423220642404
Thiago Youssef <thiagodyoussef@gmail.com>
2021-12-10 10:48:08 -0300

    Add new website email template

--- lib/absmultimarcas.rb
+++ lib/absmultimarcas.rb
@@ -17,22 +17,22 @@ module Absmultimarcas
   end
   class F1SalesCustom::Email::Parser
     def parse
-      parsed_email = @email.body.colons_to_hash(/(Nome Completo|Email|Telefone|Celular|Melhor forma de iniciar a negociacao|Mensagem|Marca|Modelo|Ano|Versao|Preco|Cambio).*?:/, false)
+      parsed_email = @email.body.colons_to_hash(/(Nome|E-mail|Tel|Veículo|Ano|Cor|Placa|Preço).*?:/, false)
       source = F1SalesCustom::Email::Source.all[0]
-      product = "#{parsed_email['marca'].capitalize} #{parsed_email['modelo'].capitalize}"
-      description = "Versão: #{parsed_email['versao']} - Ano: #{parsed_email['ano']} - Valor: #{parsed_email['preco']}"
-      message = "#{parsed_email['melhor_forma_de_iniciar_a_negociacao'].gsub('_', ' ')} - #{parsed_email['mensagem']}"
+      description = "Ano: #{parsed_email['ano'].split.join} - Cor: #{parsed_email['cor']} - Placa: #{parsed_email['placa']} - Valor: #{parsed_email['preo'].split[0..1].join(' ')}"
+
+      message = @email.body.split('ABS MULTIMARCAS,').last.split('Nome:').first.strip
 
       {
         source: {
           name: source[:name]
         },
         customer: {
-          name: parsed_email['nome_completo'],
-          phone: parsed_email['celular'].tr('^0-9', ''),
+          name: parsed_email['nome'],
+          phone: parsed_email['tel'].split[0..2].join.tr('^0-9', ''),
           email: parsed_email['email']
         },
-        product: product,
+        product: parsed_email['veculo'],
         message: message,
         description: description
       }
--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -1,15 +1,13 @@
 require 'ostruct'
 require 'byebug'
-require "f1sales_custom/parser"
-require "f1sales_custom/source"
 
 RSpec.describe F1SalesCustom::Email::Parser do
   context 'when came from the website' do
     let(:email) do
       email = OpenStruct.new
       email.to = [email: 'website@absmultimarcas.f1sales.net']
-      email.subject = 'Formulário de Proposta de Veículo | ABS MULTIMARCAS | https://www.absmultimarcas.com.br'
-      email.body = "\n\nFormulário de Proposta de Veículo \n Nome Completo: Teste Lead Site ABS MULTIMARCAS \n Email: Absmultimarcassjc@gmail.com \n Telefone: 13) 9313-13131 \n Celular: (19) 9969-28688 \n Melhor forma de iniciar a negociacao: Trocar_meu_veículo_por_um_novo_e_ainda_receber_um_valor_em_dinheiro \n Mensagem: Agora vai funcionar direitinho... \n Marca: RENAULT \n Modelo: LOGAN \n Ano: 2019/2020 \n Versao: 1.0 12V SCE FLEX LIFE MANUAL \n Preco: 53.900,00 \n Cambio: Manual \n Portas: 4 \n Combustivel: Gasolina e álcool \n Km: 45.127 \n Final da Placa: 2 \n Opcionais: Ar condicionado, Travas elétricas, Vidros elétricos, Freio ABS, Air bag do motorista, Desembaçador traseiro, Air bag duplo, Ar quente, Encosto de cabeça traseiro, Direção Elétrica, Distribuição eletrônica de frenagem, Pára-choques na cor do veículo \n Observacoes Adicionais: ?Com uma vasta gama de veículos em estoque, a ABS MULTIMARCAS tem opções para todos os gostos. Ajudamos os clientes a encontrar os melhores veículos do mercado desde 2000. Está procurando um RENAULT LOGAN 1.0 12V SCE FLEX LIFE MANUAL? Nossa equipe está pronta para auxiliar você em todas as etapas do processo. \n Loja: ABS MULTIMARCAS \n Url do Site: https://www.absmultimarcas.com.br \n Url do Formulário: https://www.absmultimarcas.com.br/estoque/veiculo/27913322/renault_logan_1.0-12v-sce-flex-life-manual"
+      email.subject = 'Proposta para o carro usado AUDI A3 1.8 20V 150CV TURBO GASOLINA 4P AUTOMÁTICO 2002'
+      email.body = "WebMotors :: Lead de veículo usado   \n  \n   \n   \n   (http://www.webmotors.com.br/?utm_source=WebMotors&utm_medium=Email_Transacional&utm_campaign=Proposta/Carro/Usado-PJ-142)    \n  \n \n  Proposta para seu Carro \n  \n \n  Responda seus leads pela Plataforma do Revendedor! \nPara garantir um melhor controle de todas as negociações e manter o histórico de cada venda, a partir de 01/02 este e-mail deixará de trazer o conteúdo das propostas recebidas, levando diretamente para responder na Plataforma do Revendedor. \n Clique aqui e responda este lead agora mesmo. (http://webmotors.vmotors.com.br/gestao-leads/central-de-mensagens) \n  \n\n ABS MULTIMARCAS, \n  \nOlá, fiz uma simulação de financiamento para este veículo e estou interessado nele. Por favor entre em contato. Tenho interesse em realizar a negociação através do Car Delivery. \n  Nome: \n                                    Peter \nE-mail:\n                                        peterjoao.pj@gmail.com \nTel.: (\n                                            12)\n                                                99722-0737 \n \n  \n  \n \n  DADOS DO VEÍCULO ANUNCIADO \n  \n \n  \n   \n  Veículo: \n                                                AUDI A3 1.8 20V 150CV TURBO GASOLINA 4P AUTOMÁTICO \nAno:\n                                                    2002 /\n                                                        2002 \nCor:\n                                                            Prata \n Placa:\n                                                                FAF6767 \n Preço: R$\n                                                                    25900.00 \n  \n   \n   Veja o anúncio >> (http://www.webmotors.com.br/c/39071136?utm_source=WebMotors&utm_medium=Email_Transacional&utm_campaign=Proposta/Carro/Usado-PJ-142) \n \n  \n Lembre-se de apagar a placa informada ao lado, caso não queira divulgá-la ao interessado. \nConfira também nossas dicas para uma venda segura! (http://dicas.webmotors.com.br/dicas-de-venda/venda-segura?utm_source=WebMotors&utm_medium=Email_Transacional&utm_campaign=Proposta/Carro/Usado-148) \n  \n  \n  \n \n Veículos anunciados em sites do Grupo WebMotors podem aparecer em mais de um classificado \n   \n   (http://www.webmotors.com.br/?utm_source=WebMotors&utm_medium=Email_Transacional&utm_campaign=Proposta-Financiamento-wm)    (http://www.meucarango.com.br/?utm_source=WebMotors&utm_medium=Email_Transacional&utm_campaign=Proposta-Financiamento-wm)    (http://www.compreauto.com.br/?utm_source=WebMotors&utm_medium=Email_Transacional&utm_campaign=Proposta-Financiamento-wm)  \n  \n   \n  Acompanhe o WebMotors \n         \n    \n  Fale Conosco \n    Dúvidas e perguntas (http://atendimento.webmotors.com.br/perguntas-frequentes?utm_source=WebMotors&utm_medium=Email_Servicos&utm_campaign=agenda/carro/seguro) \nfrequentes \n  \n  \n \n  Essa mensagem foi enviada em 09/12/2021 | 22:01 \nCadastre o domínio @webmotors.com.br como remente confiável. \nWebMotors S.A. Todos os direitos reservados"
 
       email
     end
@@ -21,27 +19,27 @@ RSpec.describe F1SalesCustom::Email::Parser do
     end
 
     it 'contains name' do
-      expect(parsed_email[:customer][:name]).to eq('Teste Lead Site ABS MULTIMARCAS')
+      expect(parsed_email[:customer][:name]).to eq('Peter')
     end
 
     it 'contains phone' do
-      expect(parsed_email[:customer][:phone]).to eq('19996928688')
+      expect(parsed_email[:customer][:phone]).to eq('12997220737')
     end
 
     it 'contains email' do
-      expect(parsed_email[:customer][:email]).to eq('Absmultimarcassjc@gmail.com')
+      expect(parsed_email[:customer][:email]).to eq('peterjoao.pj@gmail.com')
     end
 
     it 'contains product' do
-      expect(parsed_email[:product]).to eq('Renault Logan')
+      expect(parsed_email[:product]).to eq('AUDI A3 1.8 20V 150CV TURBO GASOLINA 4P AUTOMÁTICO')
     end
 
     it 'contains message' do
-      expect(parsed_email[:message]).to eq('Trocar meu veículo por um novo e ainda receber um valor em dinheiro - Agora vai funcionar direitinho...')
+      expect(parsed_email[:message]).to eq('Olá, fiz uma simulação de financiamento para este veículo e estou interessado nele. Por favor entre em contato. Tenho interesse em realizar a negociação através do Car Delivery.')
     end
 
     it 'contains description' do
-      expect(parsed_email[:description]).to eq('Versão: 1.0 12V SCE FLEX LIFE MANUAL - Ano: 2019/2020 - Valor: 53.900,00')
+      expect(parsed_email[:description]).to eq('Ano: 2002/2002 - Cor: Prata - Placa: FAF6767 - Valor: R$ 25900.00')
     end
   end
 end
